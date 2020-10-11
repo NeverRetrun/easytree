422dfb0e0c8b92208e26aee694c6c72f8e06d766
crynocry <493361889@qq.com>
2020-10-11 19:56:24 +0800

    完善部分Node节点方法

--- tests/TreeNodeTest.php
+++ tests/TreeNodeTest.php
@@ -1,9 +1,37 @@
 <?php declare(strict_types=1);
 
 
+use EasyTree\Adapter\DefaultAdapter;
 use PHPUnit\Framework\TestCase;
 
 final class TreeNodeTest extends TestCase
 {
+    use TreeDataTrait;
+
+    protected $data = [
+        'id'        => 1,
+        'parent_id' => 2,
+        'data'      => 'test',
+    ];
+
+    public function getNode()
+    {
+        return new \EasyTree\Tree\Node(
+            1,
+            2,
+            DefaultAdapter::source($this->data),
+            $this->data,
+            'children'
+        );
+    }
+
+    public function testToArray()
+    {
+        $this->assertEquals(
+            $this->getNode()->toArray(),
+            $this->data
+        );
+    }
+
 
 }
--- tests/TreeTest.php
+++ tests/TreeTest.php
@@ -6,71 +6,7 @@ use EasyTree\Tree\Tree;
 
 final class TreeTest extends TestCase
 {
-    public $foo = [
-        ['id' => 1, 'name' => '食物', 'parent_id' => 0],
-        ['id' => 2, 'name' => '车辆', 'parent_id' => 0],
-        ['id' => 3, 'name' => '面条', 'parent_id' => 1],
-        ['id' => 4, 'name' => '饮料', 'parent_id' => 1],
-        ['id' => 5, 'name' => '矿泉水', 'parent_id' => 4],
-        ['id' => 6, 'name' => '校车', 'parent_id' => 2],
-    ];
-
-    public $fooSubset = [
-        ['id' => 1, 'name' => '食物', 'parent_id' => 0, 'subset' => 2],
-        ['id' => 2, 'name' => '车辆', 'parent_id' => 0, 'subset' => 1],
-        ['id' => 3, 'name' => '面条', 'parent_id' => 1, 'subset' => 0],
-        ['id' => 4, 'name' => '饮料', 'parent_id' => 1, 'subset' => 1],
-        ['id' => 5, 'name' => '矿泉水', 'parent_id' => 4, 'subset' => 0],
-        ['id' => 6, 'name' => '校车', 'parent_id' => 2, 'subset' => 0],
-    ];
-
-    public $fooTree = [
-        [
-            'id'        => 1,
-            'name'      => '食物',
-            'parent_id' => 0,
-            'children'  => [
-                [
-                    'id'        => 3,
-                    'name'      => '面条',
-                    'parent_id' => 1,
-                ],
-                [
-                    'id'        => 4,
-                    'name'      => '饮料',
-                    'parent_id' => 1,
-                    'children'  => [
-                        [
-                            'id'        => 5,
-                            'name'      => '矿泉水',
-                            'parent_id' => 4,
-                        ],
-                    ]
-                ],
-            ],
-        ],
-        [
-            'id'        => 2,
-            'name'      => '车辆',
-            'parent_id' => 0,
-            'children'  => [
-                [
-                    'id'        => 6,
-                    'name'      => '校车',
-                    'parent_id' => 2,
-                ],
-            ]
-        ],
-    ];
-
-    public function getTree()
-    {
-        return (new \EasyTree\Tree\TreeBuilder($this->foo))
-            ->setIdKey('id')
-            ->setParentKey('parent_id')
-            ->setChildrenKey('children')
-            ->build();
-    }
+    use TreeDataTrait;
 
     public function searchOriginData($id)
     {
